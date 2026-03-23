ace70b90d7a4dae659cd354ddf454849258aa1a2
Luís <luisprinces950@gmail.com>
2026-03-23 00:09:49 -0300

    Update Cardapio

--- src/main/java/com/luis/javapos/modulo2/Cardapio.java
+++ src/main/java/com/luis/javapos/modulo2/Cardapio.java
@@ -1,4 +1,15 @@
 package com.luis.javapos.modulo2;
 
+import java.util.List;
+
 public class Cardapio {
+    private List<Produto> produtos;
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
 }
