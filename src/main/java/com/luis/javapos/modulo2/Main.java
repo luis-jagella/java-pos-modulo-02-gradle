7a55ae1dd066d4c97ca8a22acf7e3ff902301ee0
Luís <luisprinces950@gmail.com>
2026-03-23 00:18:57 -0300

    Initialize sample products and Cardapio in Main

--- src/main/java/com/luis/javapos/modulo2/Main.java
+++ src/main/java/com/luis/javapos/modulo2/Main.java
@@ -1,4 +1,17 @@
 package com.luis.javapos.modulo2;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Produto> produtos = List.of(
+                new Produto("Pão com Frango", new BigDecimal("18.50")),
+                new Produto("Hot Dog Paulista", new BigDecimal("20.60")),
+                new Produto("Brownie", new BigDecimal("28.00"))
+        );
+
+        Cardapio cardapio = new Cardapio(produtos);
+
+    }
 }
