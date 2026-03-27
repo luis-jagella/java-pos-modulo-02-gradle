9c15d907b763b34f53ff69e24da1cc58263088e9
Luís <luisprinces950@gmail.com>
2026-03-26 23:10:29 -0300

    Update Produto.java

--- src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/Produto.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/Produto.java
@@ -1,4 +1,30 @@
 package com.luis.javapos.modulo2.collections.aula01_overview;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private Categoria categoria;    // TODO: Em desenvolvimento...
+
+    public Produto(String nome, Categoria categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public String getNome() { return nome; }
+    public double getPreco() { return preco; }
+    public Categoria getCategoria() { return categoria; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setPreco(double preco) { this.preco = preco; }
+    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", categoria=" + categoria +
+                '}';
+    }
 }
