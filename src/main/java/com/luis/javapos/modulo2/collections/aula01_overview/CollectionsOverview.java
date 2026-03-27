ec40a8301aa9ccda34505592d51c78b49bcce41d
Luís <luisprinces950@gmail.com>
2026-03-26 23:21:41 -0300

    Update Collections - List

--- src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
@@ -1,4 +1,18 @@
 package com.luis.javapos.modulo2.collections.aula01_overview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionsOverview {
+
+    public static void run() {
+
+        // ♦ List (Permite duplicatas)
+        List<Produto> lista = new ArrayList<>();
+        lista.add(new Produto("X-tudão", 25, Categoria.LANCHE));
+        lista.add(new Produto("X-tudão", 25, Categoria.LANCHE));
+
+        System.out.println("LIST: ");
+        lista.forEach(System.out::println);
+    }
 }
