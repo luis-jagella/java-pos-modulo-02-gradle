d14f4eb0d886e593a532efad71b3ad458829f87d
Luís <luisprinces950@gmail.com>
2026-03-29 01:21:36 -0300

    Make produtos private; listarProdutos returns list

--- src/main/java/com/luis/javapos/modulo2/collections/aula02_cli/service/CardapioService.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula02_cli/service/CardapioService.java
@@ -6,21 +6,11 @@ import com.luis.javapos.modulo2.collections.aula02_cli.model.Produto;
 import java.util.ArrayList;
 import java.util.List;
 
-/* TODO: Tinha problema de excesso de responsas nessa class, pois eu estava tentando
-    misturar coisas da classe produto, sendo que eu somente precisava criar
-     um novo "Produto" com oque eu estava recebendo por parâmetro. */
-
 public class CardapioService {
 
-    List<Produto> produtos = new ArrayList<>();
+    private List<Produto> produtos = new ArrayList<>();
 
-    public void setProduto(String nomeProduto, double precoProduto, Categoria categoria) {
-        Produto produto = new Produto(nomeProduto, precoProduto, categoria);
-    }
-
-    public void listarProdutos() {
-        produtos.forEach(System.out::println);
-    }
+    public List<Produto> listarProdutos() { return produtos; }
 
     public void adicionarProduto(String nomeProduto, double precoProduto, Categoria categoria) {
         Produto produto = new Produto(nomeProduto, precoProduto, categoria);
