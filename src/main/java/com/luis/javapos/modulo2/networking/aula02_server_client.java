11ed01ba41ad689f6472b384f28cad0319541ac9
Luís <luisprinces950@gmail.com>
2026-04-12 23:52:59 -0300

    feat: comentários sobre a futura implementação da aula 02

--- src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
@@ -1,4 +1,6 @@
 package com.luis.javapos.modulo2.networking;
 
 public class aula02_server_client {
+    // TODO: Rever essa aula antes de realizar!
+    // TODO: Aula referente a criação do servidor com as integrações https.
 }
