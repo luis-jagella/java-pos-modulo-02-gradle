6ce187293a1ed13bb23a8c7b119de3c269e4dd25
Luís <luisprinces950@gmail.com>
2026-04-14 23:43:50 -0300

    fix: remoção de imports desnecessários.

--- src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
@@ -1,12 +1,5 @@
 package com.luis.javapos.modulo2.networking;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class aula01_socket {
 
     public static void main(String[] args) {
