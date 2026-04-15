7f3262348bbc9b4e939ce545a760bed1997ebe2f
Luís <luisprinces950@gmail.com>
2026-04-14 23:43:29 -0300

    fix: remoção de boilerplate

--- src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
@@ -11,33 +11,14 @@ public class aula01_socket {
 
     public static void main(String[] args) {
 
-        //String cep = "01001000";
-        //String urlParaChamada = "https://viacep.com.br/ws/" + cep + "/json/";
+        // String cep = "01001000";
+        // String urlParaChamada = "https://viacep.com.br/ws/" + cep + "/json/";
 
         try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(urlParaChamada))
-                    .build();
-
-            System.out.println("Conectando ao ViaCEP...");
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            String jsonResposta = response.body();
-            System.out.println("JSON Bruto recebido: " + jsonResposta);
-
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-            Object enderecoObjeto = gson.fromJson(jsonResposta, Object.class);
-
-            System.out.println("\n--- Teste de Conversão Gson ---");
-            System.out.println(gson.toJson(enderecoObjeto));
-            System.out.println("Status da conexão: " + response.statusCode());
+            //http
 
         } catch (Exception e) {
-            System.out.println("Erro ao tentar conectar ou converter: " + e.getMessage());
-            e.printStackTrace();
+            //exception
         }
     }
 }
