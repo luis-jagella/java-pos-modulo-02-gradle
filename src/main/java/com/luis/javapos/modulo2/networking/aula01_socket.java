fcef47bbe8a2400b8a5bea19080a076ee361d02b
Luís <luisprinces950@gmail.com>
2026-04-17 00:00:35 -0300

    update cep

--- src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula01_socket.java
@@ -9,7 +9,7 @@ public class aula01_socket {
 
     public static void main(String[] args) {
 
-        String cep = "01001000";
+        //String cep = "01001000";
         String urlParaChamada = "https://viacep.com.br/ws/" + cep + "/json/";
 
         try {
