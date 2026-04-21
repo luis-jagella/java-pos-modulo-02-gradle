70cb48944aeee66d4453fba69ec7a2739dd92442
Luís <luisprinces950@gmail.com>
2026-04-20 23:59:29 -0300

    fix: remoção de comentários.

--- src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
@@ -6,7 +6,6 @@ public class CollectionsOverview {
 
     public static void run() {
 
-        // ♦ List (Permite duplicatas)
         List<Produto> lista = new ArrayList<>();
         lista.add(new Produto("X-tudão", 25, Categoria.LANCHE));
         lista.add(new Produto("X-tudão", 25, Categoria.LANCHE));
@@ -14,7 +13,6 @@ public class CollectionsOverview {
         System.out.println("LIST: ");
         lista.forEach(System.out::println);
 
-        // ♦ Set (Remove as duplicatas)
         Set<String> nomes = new HashSet<>();
         nomes.add("Pizza da italiana");
         nomes.add("Pizza da italiana");
@@ -22,7 +20,6 @@ public class CollectionsOverview {
         System.out.println("\nSET:");
         nomes.forEach(System.out::println);
 
-        // ♦ TreeSet (Ordenado)
         Set<String> ordenado = new TreeSet<>();
         ordenado.add("Z");
         ordenado.add("A");
@@ -30,14 +27,12 @@ public class CollectionsOverview {
         System.out.println("\nTREESET: ");
         ordenado.forEach(System.out::println);
 
-        // ♦ Map (chave/valor)
         Map<String, Produto> mapa = new HashMap<>();
         mapa.put("1", new Produto("Monster branco", 8, Categoria.BEBIDA));
 
         System.out.println("\nMAP:");
         mapa.forEach((k , v) -> System.out.println(k + " -> " + v));
 
-        // ♦ LinkedList
         List<String> fila =  new LinkedList<>();
         fila.add("Pedido 01");
         fila.add("Pedido 02");
