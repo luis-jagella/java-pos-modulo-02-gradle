6e3d4058c47a083bf1853a46b36f08d1fc5e815c
Luís <luisprinces950@gmail.com>
2026-04-22 23:35:32 -0300

    fix: remoção de import "*", pois não é profissional, apenas para estudos.

--- src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
@@ -1,6 +1,13 @@
 package com.luis.javapos.modulo2.collections.aula01_overview;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class CollectionsOverview {
 
