0ae9a841354f7fd2684f90893599c49ab5c5172b
Luís <luisprinces950@gmail.com>
2026-04-22 23:37:03 -0300

    fix: substituição de variáveis.

--- src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
+++ src/main/java/com/luis/javapos/modulo2/collections/aula01_overview/CollectionsOverview.java
@@ -13,33 +13,33 @@ public class CollectionsOverview {
 
     public static void run() {
 
-        List<Produto> lista = new ArrayList<>();
-        lista.add(new Produto("X-tudo com ovo", 25, Categoria.LANCHE));
-        lista.add(new Produto("X-tudo com frango", 25, Categoria.LANCHE));
-        lista.add(new Produto("X-tudo com carne", 25, Categoria.LANCHE));
+        List<Produto> produtos = new ArrayList<>();
+        produtos.add(new Produto("X-tudo com ovo", 25, Categoria.LANCHE));
+        produtos.add(new Produto("X-tudo com frango", 25, Categoria.LANCHE));
+        produtos.add(new Produto("X-tudo com carne", 25, Categoria.LANCHE));
 
         System.out.println("LIST: ");
-        lista.forEach(System.out::println);
+        produtos.forEach(System.out::println);
 
-        Set<String> nomes = new HashSet<>();
-        nomes.add("Pizza de 4 queijos");
-        nomes.add("Pizza de calabresa");
+        Set<String> nomesProdutos = new HashSet<>();
+        nomesProdutos.add("Pizza de 4 queijos");
+        nomesProdutos.add("Pizza de calabresa");
 
         System.out.println("\nSET:");
-        nomes.forEach(System.out::println);
+        nomesProdutos.forEach(System.out::println);
 
-        Set<String> ordenado = new TreeSet<>();
-        ordenado.add("Z");
-        ordenado.add("A");
+        Set<String> nomesOrdenados = new TreeSet<>();
+        nomesOrdenados.add("Z");
+        nomesOrdenados.add("A");
 
         System.out.println("\nTREESET: ");
-        ordenado.forEach(System.out::println);
+        nomesOrdenados.forEach(System.out::println);
 
-        Map<String, Produto> mapa = new HashMap<>();
-        mapa.put("1", new Produto("Monster branco", 8, Categoria.BEBIDA));
+        Map<String, Produto> produtosPorId = new HashMap<>();
+        produtosPorId.put("1", new Produto("Monster branco", 8, Categoria.BEBIDA));
 
         System.out.println("\nMAP:");
-        mapa.forEach((k , v) -> System.out.println(k + " -> " + v));
+        produtosPorId.forEach((k , v) -> System.out.println(k + " -> " + v));
 
         List<String> fila =  new LinkedList<>();
         fila.add("Pedido 01");
