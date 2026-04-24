691395b6585591b8b7b4ea34361191bc8bb13c0a
Luís <luisprinces950@gmail.com>
2026-04-23 23:07:29 -0300

    feat: apenas subir servidor.

--- src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
@@ -1,6 +1,24 @@
 package com.luis.javapos.modulo2.networking;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class aula02_server_client {
-    // TODO: Rever essa aula antes de realizar!
-    // TODO: Aula referente a criação do servidor com as integrações https.
+
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000)) {
+            System.out.println("Servidor rodando na porta 8000...");
+
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("Cliente conectado!");
+
+                client.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
