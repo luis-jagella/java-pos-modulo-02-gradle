f32744ea750bf2972635e8594f5eb479b8d10502
Luís <luisprinces950@gmail.com>
2026-04-26 23:40:53 -0300

    fix: servidor multithread básico.

--- src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
@@ -1,37 +1,41 @@
 package com.luis.javapos.modulo2.networking;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.InputStream;
 
 public class aula02_server_client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         try (ServerSocket server = new ServerSocket(8000)) {
             System.out.println("Server - OK | Door - 8000");
 
             while (true) {
-
                 Socket client = server.accept();
-                System.out.println("Client: " + client.getInetAddress() + "connect!");
 
-                InputStream input = client.getInputStream();
+                new Thread(() -> {
+                    try {
+                        System.out.println("Client: " + client.getInetAddress() + " connect!");
 
-                byte[] buffer = new byte[1024];
-                int bytesLidos = input.read(buffer);
+                        InputStream input = client.getInputStream();
 
-                if (bytesLidos != -1) {
-                    String request = new String(buffer, 0, bytesLidos);
-                    System.out.println("Message received: " + request);
-                }
+                        byte[] buffer = new byte[1024];
+                        int bytesLidos = input.read(buffer);
 
-                client.close();
+                        if (bytesLidos != -1) {
+                            String request = new String(buffer, 0, bytesLidos);
+                            System.out.println("Message received: " + request);
+                        }
+
+                        client.close();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).start();
             }
-
-        } catch (Exception e) {
-            System.out.println("Error | Server: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 }
