cfcb740fa0cd9facaeb87fcb4eab0566d4dacfa8
Luís <luisprinces950@gmail.com>
2026-04-26 23:44:49 -0300

    feat: resposta ao cliente.

--- src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
+++ src/main/java/com/luis/javapos/modulo2/networking/aula02_server_client.java
@@ -1,9 +1,11 @@
 package com.luis.javapos.modulo2.networking;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class aula02_server_client {
 
@@ -20,18 +22,26 @@ public class aula02_server_client {
                         System.out.println("Client: " + client.getInetAddress() + " connect!");
 
                         InputStream input = client.getInputStream();
+                        OutputStream output = client.getOutputStream();
 
                         byte[] buffer = new byte[1024];
                         int bytesLidos = input.read(buffer);
 
+                        String request = "";
                         if (bytesLidos != -1) {
-                            String request = new String(buffer, 0, bytesLidos);
+                            String request = new String(buffer, 0, bytesLidos, StandardCharsets.UTF_8);
                             System.out.println("Message received: " + request);
                         }
+
+                        // Response for client
+                        String response = "Server received: " + request;
+                        output.write(response.getBytes(StandardCharsets.UTF_8));
+                        output.flush();
 
                         client.close();
 
                     } catch (Exception e) {
+                        System.out.println("Error | Server: " + e.getMessage());
                         e.printStackTrace();
                     }
                 }).start();
