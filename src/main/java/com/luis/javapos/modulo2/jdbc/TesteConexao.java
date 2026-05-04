980bf21fa0d845e7cdbce9dc75dc988a6b79507f
Luís <luisprinces950@gmail.com>
2026-05-03 23:39:31 -0300

    feat: primeiros testes de conexão (falha).

--- src/main/java/com/luis/javapos/modulo2/jdbc/TesteConexao.java
+++ src/main/java/com/luis/javapos/modulo2/jdbc/TesteConexao.java
@@ -1,4 +1,20 @@
 package com.luis.javapos.modulo2.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TesteConexao {
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:mysql://localhost:3306/teste_jdbc";
+        String user = "root";
+        String password = "123456";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        System.out.println("Conectado com sucesso!");
+
+        conn.close();
+    }
 }
